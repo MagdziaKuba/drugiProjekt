d56c15bbb2b088421472905a34a0da9d9acb7a8c
Magda <magdakuba@onet.pl>
2022-06-22 11:57:25 +0200

    open shop page and add product to cart

---
+++ src/test/java/demoPage/tests/CheckoutTest.java
@@ -0,0 +1,15 @@
+package demoPage.tests;
+
+import demoPage.pages.HomePage;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckoutTest extends BaseTest{
+    @Test
+    public void checkoutTest(){
+        new HomePage(driver).openShopPage().openProduct("Java Selenium WebDriver").addProductToCart();
+
+    }
+
+}
